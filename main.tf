297709f006958b1a2460a31936b78779fd2205eb
GiancarloWalk <giancarlo.tupac@gmail.com>
2022-08-24 23:56:21 -0500

    hola

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ resource "ibm_pi_image" "testacc_image  "{
   pi_image_secret_key       = "0b23eac638f040ce3c3c65fea133d7ffd0ddaeeca30b0adf"
   pi_image_bucket_region    = "us-south"
   pi_image_bucket_file_name = "paytesterv1.ova.gz"
-  pi_image_storage_type     = "tier3"
+  pi_image_storage_type     = "tier1"
 }
