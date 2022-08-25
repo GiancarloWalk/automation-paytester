19319b82bd1a3ff9b53d528ed11bc95c11ccd920
GiancarloWalk <giancarlo.tupac@gmail.com>
2022-08-25 00:01:02 -0500

    update

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "ibm_cloud_api_key" {
+  description = "API Key to use"
+  default     = "ppwmHCi_1-F6f9NoiPAf8g2IJ6gnoE3DtDm0Md-OUoj6"
+}
+variable "crn" {
+  type        = string
+  default     = "crn:v1:bluemix:public:power-iaas:us-south:a/035c4a9a8dfd4a969cd9409ae37d139c:d79a6170-22cc-47a0-bede-5f141d44f6c0::"
+  description = "Power Systems Virtual Server CRN"
+}
