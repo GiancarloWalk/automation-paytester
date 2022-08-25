b328cf3958ea3a83ffe6dda2d3577709866a0158
GiancarloWalk <giancarlo.tupac@gmail.com>
2022-08-25 02:39:14 -0500

    update

--- main.tf
+++ main.tf
@@ -11,14 +11,23 @@ timestamp = formatdate("YYYYMMDDhhmmss", timestamp())
 #  pi_image_secret_key       = "0b23eac638f040ce3c3c65fea133d7ffd0ddaeeca30b0adf"
 #  pi_image_bucket_region    = "us-south"
 #  pi_image_bucket_file_name = "paytesterv1.ova.gz"
-#  pi_image_storage_type     = "tier3"
+#  pi_image_storage_type     = "tier1"
+#  timeouts {
+#   create = "2h"
+#   delete = "2h"
+#  }
 #}
 
-
 #data "ibm_pi_image" "custom_image" {
+#  depends_on           = [ibm_pi_image.custom-image]
 #  pi_cloud_instance_id = local.cloud_instance_id
 #  pi_image_name        = "custom-image-${local.timestamp}"
 #}
+
+data "ibm_pi_image" "custom_image" {
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_image_name        = "paytester-custom"
+}
 
 resource "ibm_pi_key" "ssh_key" {
   pi_cloud_instance_id = local.cloud_instance_id
@@ -32,3 +41,55 @@ data "ibm_pi_key" "ssh_key" {
   pi_key_name          = "ssh-key-${local.timestamp}"
 }
 
+resource "ibm_pi_network" "power_pub_network" {
+  count                = 1
+  pi_network_name      = "pub-network-${local.timestamp}"  
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_type      = "pub-vlan"
+  pi_dns               = ["9.9.9.9"]
+}
+
+data "ibm_pi_network" "power_pub_network" {
+  depends_on           = [ibm_pi_network.power_pub_network]
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_name      = "pub-network-${local.timestamp}"
+}  
+
+
+resource "ibm_pi_network" "power_priv_network" {
+  count                = 1
+  pi_network_name      = "priv-network-${local.timestamp}"  
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_type      = "vlan"
+  pi_dns               = ["127.0.0.1"]
+  pi_cidr              = "192.168.0.0/24"
+  pi_gateway           = "192.168.0.1"
+  pi_ipaddress_range {
+    pi_starting_ip_address  = "192.168.0.2"
+    pi_ending_ip_address    = "192.168.0.254"
+  }
+}
+
+data "ibm_pi_network" "priv-network" {
+  depends_on           = [ibm_pi_network.power_priv_network]
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_name      = "priv-network-${local.timestamp}"
+}
+
+resource "ibm_pi_instance" "instance" {
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_memory            = var.memory
+  pi_processors        = var.processors
+  pi_instance_name     = "lpar-${local.timestamp}"
+  pi_proc_type         = var.processor_type
+  pi_image_id          = data.ibm_pi_image.custom_image.id
+  pi_key_pair_name     = data.ibm_pi_key.ssh_key.id
+  pi_sys_type          = var.sys_type
+  pi_storage_type      = var.storage_type
+  pi_network {
+    network_id = data.ibm_pi_network.pub-network.id  
+  }
+  pi_network {
+    network_id = data.ibm_pi_network.priv-network.id  
+  }
+}
--- variables.tf
+++ variables.tf
@@ -15,3 +15,34 @@ variable "ssh_key_value" {
   description = "The value of the public SSH RSA key to use when creating the instance, as defined for the selected Power Systems Virtual Server CRN"
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkMw4JstyNXBNqW9GjFN2dPRawwroCR2US5vf0O06rW/xrjudMJnOIGvUOF2kAU0oQezWV6qDTr8tfCXQYK5OKhF8TLWuegRDueErseVSo7Ehm8YWACTgrEHvrxnOyRpqib5WY6oa5+gCrkCR63RSFFpmwhNxPAwIx++A1RFqUzXwWjlWcIKAKwtPa5mUeT2cKc00cD8oihM+E2SuzlNlktkdkPe9/Yljy/+XJ2pTX/ORKGUy83uxc9n454tHwM9Q0Km9G/th8pmJKyxoYhhnwT8aVl0NTLFXPwKe8M0SQU4T3wXefLogzyBR8ySFBf+L9X+DgZNjJ8ZWl6xwkIFbzA0UdbI3XDXLUbwFOKAdFqPh5+h4x6T55lfAtDICMG8Xu2yqh4BAj11XUIW1mTx2v6F+UustjxXy5V1bjMApMtJWQ2BG3zXoAnbPJvu0DG//Sg6nurI1fkynVMUG71oir9FWqewed/C34J4Bam9rfuLvSrpBWSKpttzPUADIW6pk= gtupac"
 }
+
+variable "memory" {
+  type        = number
+  default     = 5
+  description = "The amount of memory that you want to assign to your instance in gigabytes"
+}
+variable "processors" {
+  type        = number
+  default     = 0.25
+  description = "The number of vCPUs to assign to the VM as visible within the guest Operating System"
+}
+variable "instance_name" {
+  type        = string
+  default     = "paytesteruser"
+  description = "The name to assign to the instance"
+}
+variable "processor_type" {
+  type        = string
+  default     = "shared"
+  description = "The type of processor mode in which the VM will run with 'shared', 'capped' or 'dedicated'"
+}
+variable "sys_type" {
+  type        = string
+  default     = "s922"
+  description = "The type of system on which to create the VM: 's922', 'e880', 'e980', 'e1080', or 's1022'"
+}
+variable "storage_type" {
+  type        = string
+  default     = "tier3"
+  description = "The type of storage tier to assign for storage volume performance: 'tier1' or 'tier3'"
+}
