987c39a5160f3c7fa8e4de38fd707fb1eba5e76b
Giancarlo Tupac Gutierrez <47062199+GiancarloWalk@users.noreply.github.com>
2022-12-05 17:49:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-locals {
-    timestamp = formatdate("YYYYMMDDhhmmss", timestamp())
-}
-
 #resource "ibm_pi_image" "custom-image" {
 #  pi_cloud_instance_id      = local.cloud_instance_id
 #  pi_image_name             = "custom-image-${local.timestamp}"
@@ -24,74 +20,42 @@ locals {
 #  pi_image_name        = "custom-image-${local.timestamp}"
 #}
 
-data "ibm_pi_image" "custom_image" {
+resource "ibm_pi_ike_policy" "rs-ike-policy" {
+        pi_cloud_instance_id    = local.cloud_instance_id
+        pi_policy_name          = "ike-policy"
+        pi_policy_dh_group = 20
+        pi_policy_encryption = "aes-256-cbc"
+        pi_policy_key_lifetime = 28800
+        pi_policy_preshared_key = "sample"
+        pi_policy_version = 2
+        pi_policy_authentication = "sha-256"
+    }
+
+resource "ibm_pi_ipsec_policy" "rs-ipsec-policy" {
+        pi_cloud_instance_id    = local.cloud_instance_id
+        pi_policy_name          = "ipsec-policy"
+        pi_policy_dh_group = 20
+        pi_policy_encryption = "aes-256-cbc"
+        pi_policy_key_lifetime = 3600
+        pi_policy_pfs = true
+        pi_policy_authentication = "hmac-sha-256-128"
+    }
+
+data "ibm_pi_network" "ds-network" {
+  pi_network_name = "vpn-subnet"
   pi_cloud_instance_id = local.cloud_instance_id
-  pi_image_name        = "paytester-custom"
 }
 
-resource "ibm_pi_key" "ssh_key" {
-  pi_cloud_instance_id = local.cloud_instance_id
-  pi_key_name          = "ssh-key-${local.timestamp}"
-  pi_ssh_key           = var.ssh_key_value
-}
+resource "ibm_pi_vpn_connection" "rs-vpn-connection" {
+        pi_cloud_instance_id    = local.cloud_instance_id
+        pi_vpn_connection_name  = "vpn-connection"
+        pi_ike_policy_id        = ibm_pi_ike_policy.rs-ike-policy.policy_id
+        pi_ipsec_policy_id      = ibm_pi_ipsec_policy.rs-ipsec-policy.policy_id
+        pi_vpn_connection_mode  = "policy"
+        pi_networks             = data.ibm_pi_network.ds-network.id
+        pi_peer_gateway_address = "169.46.19.234"
+        pi_peer_subnets         = ["10.177.131.192/26"]
+    }
 
-data "ibm_pi_key" "ssh_key" {
-  depends_on           = [ibm_pi_key.ssh_key]
-  pi_cloud_instance_id = local.cloud_instance_id
-  pi_key_name          = "ssh-key-${local.timestamp}"
-}
 
-resource "ibm_pi_network" "pub_network" {
-  count                = 1
-  pi_network_name      = "pub-network-${local.timestamp}"  
-  pi_cloud_instance_id = local.cloud_instance_id
-  pi_network_type      = "pub-vlan"
-  pi_dns               = ["9.9.9.9"]
-}
 
-resource "ibm_pi_network" "priv_network" {
-  count                = 1
-  pi_network_name      = "priv-network-${local.timestamp}"  
-  pi_cloud_instance_id = local.cloud_instance_id
-  pi_network_type      = "vlan"
-  pi_dns               = ["127.0.0.1"]
-  pi_cidr              = "192.168.0.0/24"
-  pi_gateway           = "192.168.0.1"
-  pi_ipaddress_range {
-    pi_starting_ip_address  = "192.168.0.2"
-    pi_ending_ip_address    = "192.168.0.254"
-  }
-}
-
-#data "ibm_pi_network" "pub_network" {
-#  depends_on           = [ibm_pi_network.pub_network]
-#  pi_cloud_instance_id = local.cloud_instance_id
-#  pi_network_name      = "pub-network-${local.timestamp}"
-#}  
-
-#data "ibm_pi_network" "priv_network" {
-#  depends_on           = [ibm_pi_network.priv_network]
-#  pi_cloud_instance_id = local.cloud_instance_id
-#  pi_network_name      = "priv-network-${local.timestamp}"
-#}
-
-data "ibm_pi_network" "power_networks" {
-    count                = 2
-    pi_network_name      = ["pub-network-${local.timestamp}","priv-network-${local.timestamp}"]
-    pi_cloud_instance_id = local.cloud_instance_id
-}
-
-resource "ibm_pi_instance" "instance" {
-  pi_cloud_instance_id = local.cloud_instance_id
-  pi_memory            = var.memory
-  pi_processors        = var.processors
-  pi_instance_name     = "paytester-${local.timestamp}"
-  pi_proc_type         = var.processor_type
-  pi_image_id          = data.ibm_pi_image.custom_image.id
-  pi_key_pair_name     = data.ibm_pi_key.ssh_key.id
-  pi_sys_type          = var.sys_type
-  pi_storage_type      = var.storage_type
-  pi_network { 
-    network_id = data.ibm_pi_network.power_networks.0.id 
-  }
-}
